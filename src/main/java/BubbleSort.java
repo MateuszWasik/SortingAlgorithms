8f55b3a489d98a35643da287ea238fbf0da7c4e8
Mateusz Wasik <mateusz.wasik@4finance.com>
2018-02-08 14:45:24 +0100

    create bubbleSort java class and test create generator for random numbers

---
+++ src/main/java/BubbleSort.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class BubbleSort {
+
+
+    private int howManyChanges;
+
+    public ArrayList<Integer> bubbleSortAlgorithm(ArrayList<Integer> notSortedArray){
+
+        return null;
+    }
+
+
+}
---
+++ src/test/java/BubbleSortTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class BubbleSortTest {
+
+    BubbleSort bubbleSort = new BubbleSort();
+    ArrayList<Integer> expectedBubbleSortedArray = new ArrayList<>();
+    ArrayList<Integer> givenRawIntegerArray = new ArrayList<>();
+
+    @Test
+    public void shouldReturnSortedArrayOfNumbers(){
+
+        //assertThat(bubbleSort.bubbleSortAlgorithm(givenRawIntegerArray).equals(expectedBubbleSortedArray));
+
+    }
+
+
+
+}
