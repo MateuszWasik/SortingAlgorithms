ad2c92a8bd5fde53a4de9b998b7b22d5aa8e06c3
Mateusz Wasik <mateusz.wasik@4finance.com>
2018-02-21 10:28:14 +0100

    Changes after CR

--- src/main/java/GenericBubbleSort.java
+++ src/main/java/GenericBubbleSort.java
@@ -2,6 +2,10 @@ import java.util.List;
 
 public class GenericBubbleSort<T extends String> {
 
+    private boolean isNextElementBigger(T firstElement, T secondElement){
+        return firstElement.compareTo(secondElement) > 0;
+    }
+
     public List<T> sort(List<T> listToSort) {
 
         if (listToSort.isEmpty()) {
@@ -10,16 +14,15 @@ public class GenericBubbleSort<T extends String> {
 
         for (int i = 0; i < listToSort.size() - 1; i++) {
 
-            boolean ifSorted = false;
-
             for (int j = 0; j < listToSort.size() - 1; j++) {
                 T temporaryValueKeeper;
+                T element = listToSort.get(j);
+                T nextElement = listToSort.get(j+1);
 
-                if (listToSort.get(j).compareToIgnoreCase(listToSort.get(j + 1)) > 0) {
+                if (isNextElementBigger(element, nextElement)) {
                     temporaryValueKeeper = listToSort.get(j);
                     listToSort.set(j, listToSort.get(j + 1));
                     listToSort.set(j + 1, temporaryValueKeeper);
-                    ifSorted = true;
                 }
             }
         }
